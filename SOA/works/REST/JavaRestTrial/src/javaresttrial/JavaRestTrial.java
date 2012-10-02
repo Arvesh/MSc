8ccfd9331d043bdd380f984b6fa0b9db19a007cc
Arvesh <arvesh9@gmail.com>
2012-10-03 00:48:02 +0400

    javaRestTrial first commit

---
+++ SOA/works/REST/JavaRestTrial/src/javaresttrial/JavaRestTrial.java
@@ -0,0 +1,20 @@
+package javaresttrial;
+
+/**
+ *
+ * @author Gulshan Bhaugeerothee <arvesh9@gmail.com>
+ */
+public class JavaRestTrial {
+
+  /**
+   * @param
+   * args
+   * the
+   * command
+   * line
+   * arguments
+   */
+  public static void main(String[] args) {
+    
+  }
+}
